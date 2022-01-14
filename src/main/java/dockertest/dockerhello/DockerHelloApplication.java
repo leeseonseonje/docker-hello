42effb97c2850d54c5f97b09dd1bb1e72e03bba8
leeseonseonje <leeseonje9323@gmail.com>
2022-01-14 23:26:09 +0900

    p

--- src/main/java/dockertest/dockerhello/DockerHelloApplication.java
+++ src/main/java/dockertest/dockerhello/DockerHelloApplication.java
@@ -2,8 +2,6 @@ package dockertest.dockerhello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class DockerHelloApplication {
